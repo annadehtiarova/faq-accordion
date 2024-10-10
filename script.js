273ad1c5275509a01387b1fbe86db73271c9f0da
annadehtiarova <60813021+annadehtiarova@users.noreply.github.com>
2024-10-10 19:58:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,11 +17,14 @@ document.addEventListener('DOMContentLoaded', function () {
             if (answer.style.display === 'block') {
                 answer.style.display = 'none'; 
                 plusIcon.style.display = 'block'; 
-                minusIcon.style.display = 'none'; 
+                minusIcon.style.display = 'none';
+                question.style.borderBottom = '1px solid hsl(292, 16%, 49%, 0.1)'; 
             } else {
                 answer.style.display = 'block'; 
                 plusIcon.style.display = 'none'; 
                 minusIcon.style.display = 'block'; 
+                question.style.borderBottom = 'none';
+                answer.style.borderBottom = '1px solid hsl(292, 16%, 49%, 0.1)';
             }
         });
     });
