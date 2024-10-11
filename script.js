347a0cdd5f17b054b862a325e82897906a9203f9
annadehtiarova <60813021+annadehtiarova@users.noreply.github.com>
2024-10-11 11:11:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,13 @@ document.addEventListener('DOMContentLoaded', function () {
     // Remove border from the last question
     questions[questions.length - 1].style.borderBottom = 'none';
 
+    // Add hover state style to all questions
+    questions.forEach((q, i) => {
+        if (q) {
+            q.classList.add('hover-state');
+        }
+    });
+
     // Reset borders for all answers
     answers.forEach((a, i) => {
         if (i !== questions.length - 1) {
@@ -20,6 +27,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Remove border from the last question
     answers[answers.length - 1].style.borderBottom = 'none';
+
     
     // Listen for a question click
     questions.forEach(question => {
@@ -38,13 +46,15 @@ document.addEventListener('DOMContentLoaded', function () {
                 answer.style.display = 'none'; 
                 plusIcon.style.display = 'block'; 
                 minusIcon.style.display = 'none';
-                question.style.borderBottom = '1px solid hsl(292, 16%, 49%, 0.1)'; 
+                question.style.borderBottom = '1px solid hsl(292, 16%, 49%, 0.1)';
+                question.classList.add('hover-state');
             } else {
                 answer.style.display = 'block'; 
                 plusIcon.style.display = 'none'; 
                 minusIcon.style.display = 'block'; 
                 question.style.borderBottom = 'none';
                 answer.style.borderBottom = '1px solid hsl(292, 16%, 49%, 0.1)';
+                question.classList.remove('hover-state');
             }
 
             
